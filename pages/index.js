2e9bd7a2a27666982333816fc243867c2c4d7f5e
jv-medeiros <jv.demed@gmail.com>
2021-07-14 01:47:15 -0300

    Aula 1 100%

---
+++ pages/index.js
@@ -0,0 +1,62 @@
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+
+function ProfileSidebar(props){
+  return(
+    <Box>
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
+
+export default function Home() {
+  const githubUser = 'jv-medeiros';
+  const pessoasFavoritas = [
+    'juunegreiros', 
+    'omariosouto', 
+    'peas',
+    'rafaballerini',
+    'marcobrunodev',
+    'felipefialho'
+  ]
+
+  return( 
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className='profileArea' style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={githubUser} />
+        </div>
+        <div className='welcomeArea' style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className='title'>
+              Bem vindo(a)
+            </h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div className='profileRelationArea' style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className='smallTitle'>
+              Pessoas da Comunidade ({pessoasFavoritas.length})
+            </h2>
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return(
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  )
+}
