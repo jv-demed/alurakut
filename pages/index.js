a4d4c959be6d3fff16449ec29db1cb4f436f05eb
jv-medeiros <jv.demed@gmail.com>
2021-07-15 01:57:03 -0300

    Aula 2 100%

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,34 @@
+import React from 'react';
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, OrkutNostalgicIconSet, AlurakutProfileSidebarMenuDefault } from '../src/lib/AlurakutCommons';
 
 function ProfileSidebar(props){
   return(
-    <Box>
+    <Box as='aside'>
       <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr />
+      <p>
+        <a className='boxLink' href={`https://github.com/${props.githubUser}`} >
+          @{props.githubUser}
+        </a>
+      </p>
+      <hr />
+      <AlurakutProfileSidebarMenuDefault/>
     </Box>
   )
 }
 
 export default function Home() {
   const githubUser = 'jv-medeiros';
+  const [comunidades, setComunidades] = React.useState([
+    {
+      id: new Date().toISOString(),
+      title: 'Eu odeio acordar cedo!',
+      image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+    }
+  ]);
   const pessoasFavoritas = [
     'juunegreiros', 
     'omariosouto', 
@@ -36,8 +52,58 @@ export default function Home() {
             </h1>
             <OrkutNostalgicIconSet />
           </Box>
+          <Box>
+            <h2 className='subTitle'>O que você deseja fazer?</h2>
+            <form onSubmit={function handleCriaComunidade(e){
+              e.preventDefault();
+              const dadosDoForm = new FormData(e.target);
+              const comunidade = {
+                id: new Date().toISOString(),
+                title: dadosDoForm.get('title'),
+                image: dadosDoForm.get('image')
+              }
+              const comunidadesAtualizadas = [...comunidades, comunidade];
+              setComunidades(comunidadesAtualizadas);
+            }}>
+              <div>
+                <input 
+                  placeholder='Qual vai ser o nome da sua comunidade?' 
+                  name='title'  
+                  area-label='Qual vai ser o nome da sua comunidade?' 
+                  type='text'
+                />
+              </div>
+              <div>
+                <input 
+                  placeholder='Coloque uma URL para usarmos de capa' 
+                  name='image'  
+                  area-label='Coloque uma URL para usarmos de capa' 
+                />
+              </div>
+              <button>
+                Criar comunidade
+              </button>
+            </form>
+          </Box>
         </div>
         <div className='profileRelationArea' style={{ gridArea: 'profileRelationsArea' }}>
+        <ProfileRelationsBoxWrapper>
+            <h2 className='smallTitle'>
+              Comunidades ({comunidades.length})
+            </h2>
+            <ul>
+              {comunidades.map((itemAtual) => {
+                return(
+                  <li key={itemAtual.id}>
+                    <a href={`/users/${itemAtual.title}`}>
+                      <img src={itemAtual.image} />
+                      <span>{itemAtual.title}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
           <ProfileRelationsBoxWrapper>
             <h2 className='smallTitle'>
               Pessoas da Comunidade ({pessoasFavoritas.length})
@@ -45,8 +111,8 @@ export default function Home() {
             <ul>
               {pessoasFavoritas.map((itemAtual) => {
                 return(
-                  <li>
-                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                  <li key={itemAtual}>
+                    <a href={`/users/${itemAtual}`}>
                       <img src={`https://github.com/${itemAtual}.png`} />
                       <span>{itemAtual}</span>
                     </a>
